153061347e00ed0f75d035db49d48ccf11ddc0ed
Jeff Reiner <theundeadghost@gmail.com>
2019-01-13 17:47:48 +0100

    :sparkles: building logic for supply / withdraw

--- src/components/EditStakePanel/index.js
+++ src/components/EditStakePanel/index.js
@@ -1,37 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { Subscribe } from "unstated";
+import { Flex, Text } from "rebass";
+import { useInput, useBoolean } from "react-hanger";
 
 import StakingContainer from "../StakingContainer";
 
-const EditStakePanel = ({ index, onClose }) => (
-  <Subscribe to={[StakingContainer]}>
-    {staking => (
-      <>
-        <button onClick={onClose}>Close Panel</button>
-        <pre>
-          <code>
-            {JSON.stringify(staking.state.validators[index], null, 2)}
-          </code>
-        </pre>
-        <button
-          onClick={() => {
-            staking.decrementLiquidStake(1000);
-            staking.increaseValidatorStake(index, 1000);
-          }}
-        >
-          Commit 1000 Stake
-        </button>
-        <button
-          onClick={() => {
-            staking.incrementLiquidStake(1000);
-            staking.decreaseValidatorStake(index, 1000);
-          }}
-        >
-          Withdraw 1000 Stake
-        </button>
-      </>
-    )}
-  </Subscribe>
-);
+const EditStakePanel = ({ index, onClose }) => {
+  const supplyInput = useInput("");
+  const withdrawInput = useInput("");
+  const [tab, setTab] = useState(0);
+
+  return (
+    <Subscribe to={[StakingContainer]}>
+      {staking => (
+        <>
+          <button onClick={onClose}>Close Panel</button>
+
+          <pre>
+            <code>
+              {JSON.stringify(staking.state.validators[index], null, 2)}
+            </code>
+          </pre>
+
+          <button onClick={() => setTab(0)}>supply</button>
+          <button onClick={() => setTab(1)}>withdraw</button>
+
+          {tab === 0 ? (
+            <>
+              <Flex justifyContent="space-between">
+                <Text>Liquid Stake</Text>
+                <Text>{staking.state.liquidStake.toLocaleString()} ATOM</Text>
+              </Flex>
+
+              <input
+                type="number"
+                value={supplyInput.value}
+                onChange={supplyInput.onChange}
+              />
+
+              <button
+                disabled={
+                  supplyInput.value > staking.state.liquidStake ? true : false
+                }
+                onClick={() => {
+                  staking.decrementLiquidStake(parseInt(supplyInput.value, 10));
+                  staking.increaseValidatorStake(
+                    index,
+                    parseInt(supplyInput.value, 10)
+                  );
+                }}
+              >
+                {supplyInput.value > staking.state.liquidStake
+                  ? "insufficient stake"
+                  : "supply stake"}
+              </button>
+            </>
+          ) : (
+            <>
+              <Flex justifyContent="space-between">
+                <Text>Bonded Validator Stake</Text>
+                <Text>
+                  {staking.state.validators[index].bondedStake.toLocaleString()}{" "}
+                  ATOM
+                </Text>
+              </Flex>
+
+              <input
+                type="number"
+                value={withdrawInput.value}
+                onChange={withdrawInput.onChange}
+              />
+
+              <button
+                disabled={
+                  withdrawInput.value >
+                  staking.state.validators[index].bondedStake
+                    ? true
+                    : false
+                }
+                onClick={() => {
+                  staking.incrementLiquidStake(
+                    parseInt(withdrawInput.value, 10)
+                  );
+                  staking.decreaseValidatorStake(
+                    index,
+                    parseInt(withdrawInput.value, 10)
+                  );
+                }}
+              >
+                {withdrawInput.value >
+                staking.state.validators[index].bondedStake
+                  ? "insufficient bonded stake"
+                  : "widthdraw stake"}
+              </button>
+            </>
+          )}
+        </>
+      )}
+    </Subscribe>
+  );
+};
 
 export default EditStakePanel;
